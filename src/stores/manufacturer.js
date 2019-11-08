7654538d58ae2b7a593abaa99503e85974073bbd
iliser <i.d.mvmii@gmail.com>
2019-11-08 17:49:14 +0800

    Show only smsConfirmed manufacturer and sort in "new" order

--- src/stores/manufacturer.js
+++ src/stores/manufacturer.js
@@ -1,2 +1,24 @@
 import { createCRUDStore } from './type/crud.js';
-export const manufacturer = createCRUDStore('manufacturer');
+import Axios from 'axios'
+export const manufacturer = createCRUDStore('manufacturer',{
+    load(store,modelName){
+        return async (skip = 0, limit = 100) => {
+            try {
+                const res = await Axios.get(`/api/crud/${modelName}?isSmsConfirmed=true&__sort=-createdAt`, {
+                    params: {
+                        __limit: limit,
+                        __skip: skip
+                    }
+                });
+                store.set(res.data);
+                return res.data;
+            } catch (err) {
+                console.log(`#${modelName.toUpperCase()} LOAD ERROR`);
+                console.log(err);
+
+                return false;
+            }
+        };
+    }
+});
+
