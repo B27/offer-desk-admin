dad5024eb5fb4df3bd3eabd9fc54c7d9b7b0bf59
iliser <i.d.mvmii@gmail.com>
2019-12-25 12:23:24 +0800

    Add ad moderation

--- src/routes/ads/_ad.svelte
+++ src/routes/ads/_ad.svelte
@@ -1,40 +1,36 @@
 <script>
+    import axios from 'axios';
     import {apiBaseUrl} from '../../config.js';
+    import { createEventDispatcher } from 'svelte';
+    
     export let ad;
-    // ad = {
-    //     photos: [
-    //         "upload_959f181525451b7f8c05d8ef56ca6cdf",
-    //         "upload_1f473bab2b8e4dfafa7874a984553fab",
-    //         "upload_c41eaba0cf7ac0c35a536734103e04fb"
-    //     ],
-    //     rating: 5,
-    //     rateCount: 4,
-    //     title: "Редиска своя очень вкусная",
-    //     description: "Мы выращиваем редиску в г Ангарске в теплицах",
-    //     category: {
-    //         name:"Надо допилить",
-    //         id:"5dce99338c13611cb6ed53de",
-    //     },
-    //     region: {
-    //         name: "Иркутск",
-    //         createdAt: "2019-11-14T12:32:05.050Z",
-    //         updatedAt: "2019-11-14T12:32:05.050Z",
-    //         id: "5dcd4944b79ae34828e79040"
-    //     },
-    //     phoneNumber: "+7 964 111-55-50",
-    //     price: "70",
-    //     manufacturer: {
-    //         name: "СНТ мичюринец",
-    //         id: "5de0ca50067f5466ebdbe86d"
-    //     },
-    //     preview: "upload_959f181525451b7f8c05d8ef56ca6cdfthumb",
-    //     createdAt: "2019-11-29T07:41:06.328Z",
-    //     updatedAt: "2019-12-01T05:19:56.805Z",
-    //     id: "5de0cb8e067f5466ebdbe86e"
-    // };
+
+    const dispatch = createEventDispatcher();
+
     let pad = ad;
+    let disable = false;
+    let error = false;
+    let changeHidden = async () => {
+        let status = !pad.hidden;
+        disable = true;
+        error = false;
+        try{
+            // console.log(ad.id);
+            const ans = await axios.patch('/api/crud/ad',{
+                id:ad.id,
+                hidden:status
+            });
+            ad.hidden = status;
+            dispatch('hide',ad.id);
+        }catch(err){
+            error = true;
+        }
+        disable = false;
+    }
+
     $: ad, pad = {
         ...ad,
+        createdAt:new Date(ad.createdAt),
         category:(ad.category || {}).name,
         region:(ad.region || {}).name,
         manufacturer:(ad.manufacturer || {}).name
@@ -45,13 +41,16 @@
     <div class="category">
         <span class="category_name" class:lose_data={!pad.category}>{pad.category || "Данные отсутствуют"}</span>
         <span class="grow"></span>
-        <button>
-            &#x2714
+        <span class="date">{pad.createdAt.toLocaleDateString("ru-RU")}</span>
+        
+        <button {disable} class:deleted={pad.hidden} class:error={error} on:click={changeHidden}>
+                &#x2716
         </button>
+        
     </div>
     <div class="photos">
         {#each ad.photos as photo}
-            <img width=128px height=128px src={`${apiBaseUrl}/api/images/${photo}`} alt="nope">
+            <img width=128px height=128px src={`${apiBaseUrl}/api/images/${photo}`} alt="nope" on:error={e => e.target.style.display = "none"}>
         {/each}
     </div>
     <div class="tittle">{ad.title}</div>
@@ -71,7 +70,10 @@
 </div>
 
 <style>
-    .lose_data{
+    .deleted{
+        color: green;
+    }
+    .lose_data,.error{
         color: rgba(128,0,0);
     }
     img{
--- src/routes/ads/index.svelte
+++ src/routes/ads/index.svelte
@@ -2,7 +2,7 @@
     import axios from 'axios';
     export async function preload() {
         try{
-            const ans = await axios.get('/api/crud/ad');
+            const ans = await axios.get('/api/crud/ad?__limit=10&__populate=region manufacturer category');
             return {ads:ans.data}
         }catch(err){}
         return {ads:[]}
@@ -11,7 +11,63 @@
 </script>
 <script>
     export let ads;
+
+    let offset = 0;
+    let limit = 10;
+    let end = ads.length < limit;
+    async function reload(){
+        const ans = await axios.get(`/api/crud/ad?__skip=${offset}&__limit=${limit}&__populate=region manufacturer category`)
+        ads = ans.data;
+        end = ads.length < limit;
+    }
+    async function next(){
+        offset += limit;
+        reload();
+    }
+    async function back(){
+        offset -= limit;
+        reload();
+    }
+
+    async function hide(e){
+        ads = ads.filter(v => v.id != e.detail);
+    }
 </script>
+
+<svelte:head>
+    <title>Модерация объявлений</title>
+</svelte:head>
+
+<div class="pages">
+    <span class="page" class:show={offset} on:click={back}>Назад</span>
+    <span class="grow">WWW</span>
+    <span class="page" class:show={!end} on:click={next}>Вперёд</span>
+</div>
+
 {#each ads as ad}
-    <Ad {ad}/>
+    <Ad {ad} on:hide={hide}/>
 {/each}
+
+<style>
+    .pages{
+        background-color: #d3d3d3;
+        border: 1px solid gray;
+        display: flex;
+    }
+    .grow{
+        padding:0.25em;
+        flex-grow: 1;
+        color:transparent;
+    }
+    .page{
+        padding:0.25em;
+        cursor: pointer;
+        display: none;
+    }
+    .show{
+        display: block;
+    }
+    .page:hover{
+        background: #e7e7e746;
+    }
+</style>
